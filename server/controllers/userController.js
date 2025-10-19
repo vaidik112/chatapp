f21194c39cd8f5b50e0e8a7cf56fc39b643e2e9c
Vaidik <vaidikkumar01@example.com>
2025-10-19 12:42:50 +0530

    new files

---
+++ server/controllers/userController.js
@@ -0,0 +1,51 @@
+import User from "../lib/models/user.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/utils.js";
+// signup a new user
+export const signup = async (req,res) =>{
+    const {fullName, email, password, bio} = req.body;
+    try {
+        if(!fullName || !email || !password || !bio)
+        {
+            return res.json({success: false, message: "missing details"});
+        }
+        const user = await User.findOne({email});
+        if(user)
+        {
+            return res.json({success: false,message: "account already exists"});
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password,salt);
+        const newUser = await User.create({
+            fullName, email, password: hashedPassword,bio
+        });
+        const token = generateToken(newUser._id);
+        res.json({success: true, userData: newUser,token, message: "Account Created Successfully"})
+    } catch (error) {
+        console.log("signup error:",error);
+        res.json({success: false, message: error.message})
+    } 
+}
+// controller
+export const login =  async(req,res) =>{
+    try {
+        const {email,password} = req.body;
+        const userData = await User.findOne({email});
+        const isPasswordCorrect = await bcrypt.compare(password,userData.password);
+        if(!isPasswordCorrect)
+        {
+            res.json({success: false, message:"invalid credentials"});
+        }
+        const token = generateToken(userData._id);
+        res.json({
+            success:true, userData, token, message: "Account created successfully"
+        });
+    } catch (error) {
+            console.log("login error:",error);
+            res.json({success: false, message: error.message}) 
+    }
+}
+// controller to check if user is authenticated
+export const checkAuth = (req,res) =>{
+    res.json({ssuccess: true,user: req.user});
+}
---
+++ server/lib/utils.js
@@ -0,0 +1,9 @@
+import jwt from "jsonwebtoken";
+
+// function to generate a token for user
+export const generateToken = (userId) =>{
+   const token = jwt.sign({userId},process.env.JWT_SECRET);
+   return token;
+}
+
+
